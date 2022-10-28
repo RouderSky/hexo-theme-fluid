0d2d4fa6c396c23d3c9dfc523adff7b2081e1938
Rouder_HZ <1158261439@qq.com>
2022-10-29 01:36:38 +0800

    整理音乐播放器

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,19 +1,12 @@
 // APlayer音乐播放器
-hexo.extend.injector.register('body_end', `
-  <div id="aplayer"></div>
-  <link defer rel="stylesheet" href="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.css" />
-  <script src="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.js"></script>
-  <script defer src="/js/aplayer.js"></script>
-`);
-
 // hexo.extend.injector.register('body_end', `
-// <link rel="stylesheet" href="APlayer.min.css">
-// <div id="aplayer"></div>
-// <script src="APlayer.min.js"></script>
+//   <div id="aplayer"></div>
+//   <link defer rel="stylesheet" href="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.css" />
+//   <script src="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.js"></script>
+//   <script defer src="/js/aplayer.js"></script>
 // `);
 
+// 网易云音乐
 // hexo.extend.injector.register('body_end', `
-//   <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=450 src="//music.163.com/outchain/player?type=0&id=4980157066&auto=1&height=430"></iframe>
+// <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=528283&auto=1&height=66"></iframe>
 // `);
-
-
