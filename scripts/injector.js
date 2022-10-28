435adb2278e8772cbb606f3b6b505ed5a346cf23
Rouder_HZ <1158261439@qq.com>
2022-10-29 01:02:29 +0800

    aplayer音乐播放器

---
+++ scripts/injector.js
@@ -0,0 +1,15 @@
+hexo.extend.injector.register('body_end', `
+  <div id="aplayer"></div>
+  <link defer rel="stylesheet" href="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.css" />
+  <script src="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.js"></script>
+  <script defer src="/js/aplayer.js"></script>
+`);
+
+
+// hexo.extend.injector.register('body_end', `
+// <link rel="stylesheet" href="APlayer.min.css">
+// <div id="aplayer"></div>
+// <script src="APlayer.min.js"></script>
+// `);
+
+
